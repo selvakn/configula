dd48354c5f986e802fb4f151c6106b30cd92ebc4
Selva <k.n.selvakumar@gmail.com>
2009-11-19 18:31:01 +0530

    wip

--- lib/configula/configula.rb
+++ lib/configula/configula.rb
@@ -1,3 +1,50 @@
+class ConfigulaNew
+  @@locked = false
+  
+  def self.locked?
+    @@locked
+  end
+  
+  def self.lock
+    @@locked = true
+  end
+  
+  def self.unlock
+    @@locked = false
+  end
+  
+  def set(key, value)
+    send("#{key}=", value)
+  end
+  
+  def method_missing(method_name, *args)
+    return if args.empty? and self.class.locked? # trying to read after locking
+    
+    if args.empty?
+      new_config = Configula.new
+      (class <<self; self; end).send(:define_method, method_name, lambda { new_config })
+      return new_config
+    end
+    
+    return raise "config locked" if self.class.locked? # trying to set value after locking
+    
+    value = lambda do 
+      val = args.first
+      case
+      when val.kind_of?(Proc)
+        val.call
+      else
+        val
+      end
+    end
+    
+    method_name = method_name.to_s
+    method_name = method_name[-1,1] == '=' ? method_name[0..-2] : method_name
+    
+    (class <<self; self; end).send(:define_method, method_name, value)
+  end
+end
+
 class Configula
   
   def initialize(children_hash={})
