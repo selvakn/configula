56ffc10aa5180fd3bf6626f5f438ce94a0bcd945
Selva <k.n.selvakumar@gmail.com>
2009-12-27 00:42:02 +0530

    need to fix chaining

--- lib/configula/configula.rb
+++ lib/configula/configula.rb
@@ -1,46 +1,60 @@
 class Configula
-  @@locked = false
+  @@prepared = false
   
-  def self.locked?
-    @@locked
+  def self.prepared?
+    @@prepared
   end
   
-  def self.lock
-    @@locked = true
+  def self.prepare
+    config = new
+    config.instance_variables.each do |name|
+      actual_value = config.instance_variable_get(name)
+      # config.instance_variable_set(name, actual_value.call) if actual_value.kind_of?(Proc)
+    end
+    
+    @@prepared = true
+    config
   end
   
-  def self.unlock
-    @@locked = false
+  def self.reset
+    @@prepared = false
   end
   
   def set(key, value)
-    send("#{key}=", value)
+    instance_variable_set "@#{key}", value
+  end
+  
+  def get(key)
+    instance_variable_get("@#{key}")
   end
   
   def method_missing(method_name, *args)
-    return if args.empty? and self.class.locked? # trying to read after locking
-    raise "config locked" if self.class.locked? # trying to set value after locking
+    return get(method_name) if args.empty? and self.class.prepared?
+    raise "config value cannot be changed now" if self.class.prepared? # trying to set value after prepared
     
-    
+    # Multilevel Chaining
     if args.empty?
       new_config = Configula.new
-      (class <<self; self; end).send(:define_method, method_name, lambda { new_config })
+      set method_name, new_config
       return new_config
     end
     
-    value = lambda do 
-      val = args.first
-      case
-      when val.kind_of?(Proc)
-        val.call
-      else
-        val
-      end
-    end
+    
+    # value = lambda do 
+    #   val = args.first
+    #   case
+    #   when val.kind_of?(Proc)
+    #     val.call
+    #   else
+    #     val
+    #   end
+    # end
+    
     
     method_name = method_name.to_s
     method_name = method_name[-1,1] == '=' ? method_name[0..-2] : method_name
     
-    (class <<self; self; end).send(:define_method, method_name, value)
+    set method_name, args.first
   end
+  
 end
--- spec/configula_spec.rb
+++ spec/configula_spec.rb
@@ -1,33 +1,53 @@
 require File.join(File.dirname( __FILE__ ), 'spec_helper')
 
 describe Configula do
-  class MyConfig < Configula
-    def initialize
-      set :string_config, "some_string_value"
-      set :proc_config, lambda{ "this is a proc: #{string_config}" }
-      
-      another_config "another_config"
-      self.config_equals = "config_equals"
-      chaining.config = "chaining config"
+  before(:each) do
+    class MyConfig < Configula
+      def initialize
+        set :string_config, "some_string_value"
+        set :proc_config, lambda{ "this is a proc: #{string_config}" }
+        
+        another_config "another_config"
+        self.config_equals = "config_equals"
+        chaining.config = "chaining config"
+      end
     end
   end
-  
-  it "should allow setting of values with set" do
-    MyConfig.new.string_config.should == "some_string_value"
+
+  after(:each) do
+    MyConfig.reset
   end
-  
-  it "should allow setting of values by calling the config as a method" do
-    MyConfig.new.another_config.should == "another_config"
+
+  describe "setting of basic values" do
+    before(:each) do
+      @config = MyConfig.prepare
+    end
+
+    it "should call the proc config when it is a proc " do
+      @config.proc_config.should == "this is a proc: some_string_value"
+    end
+
+    it "should allow values assigned with set setting of values with set" do
+      @config.string_config.should == "some_string_value"
+    end
+
+    it "should allow setting of values by calling the config as a method" do
+      @config.another_config.should == "another_config"
+    end
+
+    it "should allow setting of values by calling the config as a method=" do
+      @config.config_equals.should == "config_equals"
+    end
+
+    it "should multi step chaining config" do
+      @config.chaining.config.should == "chaining config"
+    end
+
+    it "should return nil value for unknown config" do
+      @config.some_unknown_key.should == nil
+    end
   end
-  
-  it "should allow setting of values by calling the config as a method=" do
-    MyConfig.new.config_equals.should == "config_equals"
-  end
-  
-  it "should multi step chaining config" do
-    MyConfig.new.chaining.config.should == "chaining config"
-  end
-  
+
   it "should allow overriding of the values with inherting" do
     class InheritedConfig < MyConfig
       def initialize
@@ -37,23 +57,17 @@ describe Configula do
         chaining.config = "new config chaining"
       end
     end
-    
-    InheritedConfig.new.string_config.should == "new string value"
-    InheritedConfig.new.config_equals.should == "new config equals"
-    InheritedConfig.new.chaining.config.should == "new config chaining"
+
+    config = InheritedConfig.prepare
+
+    config.string_config.should == "new string value"
+    config.config_equals.should == "new config equals"
+    config.chaining.config.should == "new config chaining"
   end
-  
-  it "should return nil value for unknown config" do
-    config = MyConfig.new
-    MyConfig.lock
-    config.string_config.should == "some_string_value"
-    config.some_unknown_key.should == nil
-    MyConfig.unlock
-  end
-  
-  it "should not allow changes without locking" do
-    MyConfig.lock
-    
+
+  it "should not allow changes after preparing" do
+    MyConfig.prepare
+
     class InheritedConfig < MyConfig
       def initialize
         super
@@ -62,30 +76,7 @@ describe Configula do
         chaining.config = "new config chaining"
       end
     end
-    
-    lambda { InheritedConfig.new }.should raise_error
-    
-    MyConfig.unlock
-  end
-  
-  describe "storing value" do
-    before(:each) do
-      MyConfig.unlock
-      @config = MyConfig.new
-      MyConfig.lock
-    end
-    
-    after(:each) do
-      MyConfig.unlock
-    end
-    
-    it "should return the config value when it is a string " do
-      @config.string_config.should == "some_string_value"
-    end
 
-    it "should call the proc config when it is a proc " do
-      @config.proc_config.should == "this is a proc: some_string_value"
-    end
+    lambda { InheritedConfig.prepare }.should raise_error
   end
-  
 end
