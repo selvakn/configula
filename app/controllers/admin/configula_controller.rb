d40ff8183eb97d7e869123aa3131f06e2102d888
Selva <k.n.selvakumar@gmail.com>
2010-01-10 20:19:46 +0530

    enigine wip

--- app/controllers/admin/configula_controller.rb
+++ app/controllers/admin/configula_controller.rb
@@ -1,5 +1,14 @@
 module Admin
   class ConfigulaController < ApplicationController
+    def index
+      render :json => AppConfig
+    end
     
+    def update
+      params["config"].each do |key, value|
+        AppConfig.set(key, value)
+      end
+      render :text => "Success"
+    end
   end
 end
--- spec/app/controllers/admin/configula_controller_spec.rb
+++ spec/app/controllers/admin/configula_controller_spec.rb
@@ -1,8 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../rails_spec_helper')
 
-describe Admin::ConfigulaController do
-  it "should do something" do
-    pending
-    raise AppConfig.inspect
+describe Admin::ConfigulaController, :type => :controller do
+  integrate_views
+
+  before(:each) do
+    @actual_config = AppConfig.to_json.dup
+  end
+  
+  it "should list all the available config" do
+    get :index
+    response.body.should == AppConfig.to_json
+  end
+  
+  it "should change the AppConfig for the given key(s) with those values" do
+    post :update, "config" => {"config1" => "config1_newvalue"}
+    AppConfig.config1.should == "config1_newvalue"
+  end
+  
+  it "should update the config" do
+    AppConfig.set("config1", "value1")
+    AppConfig.set("config2", "value2")
+    AppConfig.config1.should == "value1"
+    AppConfig.config2.should == "value2"
+
+    post :update, "config" => {"config1" => "value1", "config2" => "config2_newvalue"}
+    AppConfig.config1.should == "value1"
+    AppConfig.config2.should == "config2_newvalue"
   end
 end
