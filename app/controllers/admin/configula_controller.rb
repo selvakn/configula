de80c6798f752e7012b1363649c52fe14caade1c
Selva <k.n.selvakumar@gmail.com>
2010-01-17 00:02:05 +0530

    Showing status message on web interface

--- app/controllers/admin/configula_controller.rb
+++ app/controllers/admin/configula_controller.rb
@@ -3,6 +3,15 @@ module Admin
     
     def update
       AppConfig.reset(JSON.parse(params["config"]))
+      flash[:info] = "Config Prepared"
+
+      begin
+        AppConfig.persist
+        flash[:info] << " And Persisted"
+      rescue Configula::ConfigError => e
+        flash[:error] = "Error '#{e.message}' while persisting"
+      end
+      
       redirect_to :action => :index
     end
   end
